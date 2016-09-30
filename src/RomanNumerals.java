9f42487fae23ae50723d5c21d313256dfe448927
Bit <Bit@10.20.216.103>
2016-09-30 15:36:35 +0300

    Assignment finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,99 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		int IXCM = checkIfCharacters_IXCM_repeat(romanNum);
+		if(IXCM == 1){
+			return 1337;
+		}
+		int VLD = checkIfCharacters_VLD_repeated(romanNum);
+		if(VLD == 1){
+			return 1020304050;
+		}
+		int decimal = 0;
+		// Used in checking numbers like IV = 4
+		int lastNumber = 0;
+	
+		for(int i=romanNum.length() -1; i>=0; i--){
+			
+			char compare = romanNum.charAt(i);
+			
+			if(compare == 'M'){
+				decimal= checkNumber(1000, lastNumber, decimal);
+				lastNumber = 1000;
+			}
+			
+			if(compare == 'D'){
+				decimal= checkNumber(500, lastNumber, decimal);
+				lastNumber = 500;
+			}
+			
+			if(compare == 'C'){
+				decimal= checkNumber(100, lastNumber, decimal);
+				lastNumber = 100;
+			}
+			
+			if(compare == 'L'){
+				decimal= checkNumber(50, lastNumber, decimal);
+				lastNumber = 50;
+			}
+			
+			if(compare == 'X'){
+				decimal= checkNumber(10, lastNumber, decimal);
+				lastNumber = 10;
+			}
+			
+			if(compare == 'V'){
+				decimal= checkNumber(5, lastNumber, decimal);
+				lastNumber = 5;
+			}
+			
+			if(compare == 'I'){
+				decimal= checkNumber(1, lastNumber, decimal);
+				lastNumber = 1;
+			}
+			
+		}
 		
+		return decimal;
+		
+	}
+	
+	public static int checkNumber(int decimal, int lastNumber, int newNumber){
+		if(lastNumber > decimal){
+			return newNumber - decimal;
+		}
+		else {
+			return newNumber + decimal;
+		}
+	}
+	
+	public static int checkIfCharacters_IXCM_repeat(String romanNum){
+		
+		if(romanNum.matches("(.*)IIII(.*)")== true){
+			return 1;
+		}
+		if(romanNum.matches("(.*)XXXX(.*)")== true){
+			return 1;
+		}
+		if(romanNum.matches("(.*)CCCC(.*)")== true){
+			return 1;
+		}
+		if(romanNum.matches("(.*)MMMM(.*)")== true){
+			return 1;
+		}
+		return 0;
+	}
+	
+	public static int checkIfCharacters_VLD_repeated(String romanNum){
+		if(romanNum.matches("(.*)VV(.*)")== true){
+			return 1;
+		}
+		if(romanNum.matches("(.*)LL(.*)")== true){
+			return 1;
+		}
+		if(romanNum.matches("(.*)DD(.*)")== true){
+			return 1;
+		}
+		return 0;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,118 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_M_equals_1000() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("M");
+		assertEquals(1000, result);
 	}
-
+	
+	@Test
+	public void test_M_equals() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("M");
+		String mark = "M";
+		assertTrue(mark == "M");
+	}
+	
+	@Test
+	public void test_D_equals_500() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("D");
+		assertEquals(500, result);
+	}
+	
+	@Test
+	public void test_C_equals_100() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("C");
+		assertEquals(100, result);
+	}
+	
+	@Test
+	public void test_L_equals_50() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("L");
+		assertEquals(50, result);
+	}
+	
+	@Test
+	public void test_X_equals_10() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("X");
+		assertEquals(10, result);
+	}
+	
+	@Test
+	public void test_V_equals_5() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("V");
+		assertEquals(5, result);
+	}
+	
+	@Test
+	public void test_I_equals_1() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("I");
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void test_LV_equals_2055() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("LV");
+		assertEquals(55, result);
+	}
+	
+	@Test
+	public void test_IV_equals_4() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("IV");
+		assertEquals(4, result);
+	}
+	
+	@Test
+	public void test_MCMLXXXIV_equals_1984() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("MCMLXXXIV");
+		assertEquals(1984, result);
+	}
+	
+	@Test
+	public void test_MMXIV_equals_2014() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("MMXIV");
+		assertEquals(2014, result);
+	}
+	
+	@Test
+	public void test_X_cant_appear_fourtimes() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("MXXXXII");
+		assertEquals(1337, result);
+	}
+	
+	@Test
+	public void test_V_cant_appear_2times() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("MCMVVIII");
+		assertEquals(1020304050, result);
+	}
+	
+	@Test
+	public void test_XXC_Equals_100() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("XXC");
+		assertEquals(100, result);
+	}
+	
+	@Test
+	public void test_V_CantSubtracted() {
+		RomanNumerals test = new RomanNumerals();
+		int result = test.convertToInteger("VI");
+		assertEquals(6, result);
+	}
+	
 }
